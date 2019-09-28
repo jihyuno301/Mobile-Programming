5651242a53f8034e3d425915a008c51c92b85570
jihyuno301 <joh755807@gmail.com>
2019-09-28 21:50:20 +0900

    SignupActivity, MemoActivity

---
+++ E01Button/app/src/main/java/net/skhu/MainActivity.java
@@ -0,0 +1,32 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+// AppCompatActivity : 앱호환을 구현한 클래스
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.button);
+        View.OnClickListener listenerObj = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText e = (EditText)findViewById(R.id.editText);
+                CharSequence s = e.getText();
+                TextView t = (TextView)findViewById(R.id.textView);
+                t.setText(s);
+            }
+        };
+        b.setOnClickListener(listenerObj);
+
+    }
+}
---
+++ E02View/app/src/main/java/net/skhu/MainActivity.java
@@ -0,0 +1,45 @@
+package net.skhu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    // 메뉴 생성되어야 할 때 저절로 호출되는 메소드
+    public boolean onCreateOptionsMenu(Menu menu)  {
+        // inflation : XML 리소스 파일의 내용대로 Java 객체를 자동으로 생성해주는 기능
+        // R.menu.menu_main : res/menu/menu_main.xml 메뉴 리소스 파일의 id
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    // 메뉴 항목이 클릭되면 저절로 호출되는 메소드
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_signUp) {
+            // Context 클래스는 모든 액티비티 클래스의 부모
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.action_memo) {
+            Intent intent = new Intent(this, MemoActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
