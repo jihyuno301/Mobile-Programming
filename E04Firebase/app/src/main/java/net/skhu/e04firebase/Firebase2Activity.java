e30ed42581bff717884c32fbb3b23c9b046d27a1
jihyuno301 <joh755807@gmail.com>
2019-12-09 20:16:21 +0900

    Firebase RecyclerView 구현

---
+++ E04Firebase/app/src/main/java/net/skhu/e04firebase/Firebase2Activity.java
@@ -0,0 +1,14 @@
+package net.skhu.e04firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Firebase2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase2);
+    }
+}
---
+++ E04Firebase/app/src/main/java/net/skhu/e04firebase/Item2.java
@@ -0,0 +1,34 @@
+package net.skhu.e04firebase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Item2 {
+    final static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    String title;
+    Date date;
+    boolean checked;
+
+    public Item2() {
+    }
+
+    public Item2(String title, Date date) {
+        this.title = title;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getDateFormatted() {
+        return format.format(date);
+    }
+
+    public boolean isChecked() { return checked; }
+    public void setChecked(boolean checked) { this.checked = checked; }
+}
